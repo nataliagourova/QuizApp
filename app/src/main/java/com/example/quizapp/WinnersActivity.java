2afb26c6c1ff2bc2e53afae5d71a597476294746
NataliaGourova <46657216+nataliagourova@users.noreply.github.com>
2021-05-15 17:52:58 -0700

    add winners activity

---
+++ app/src/main/java/com/example/quizapp/WinnersActivity.java
@@ -0,0 +1,14 @@
+package com.example.quizapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WinnersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_winners);
+    }
+}
